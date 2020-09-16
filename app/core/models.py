8e734c65b073dc13b5ad235be5e4de362acf756c
kumar krishnamoorthy <mrkumarji@yahoo.com>
2020-09-16 16:19:34 -0500

    added user - issue with password needs research

---
+++ app/core/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
+    
+class UserManager(BaseUserManager):
+      
+       
+    def create_user(self, email, password=None, **extra_fields):
+        """ creating user record """
+        if not email:
+            raise ValueError("Email Address is not provided")
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+    
+    
+    def create_superuser(self, email, password):
+        """Creates and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    """ customer user model that support email instead username"""
+    email = models.EmailField(max_length=255, unique=True)    
+    name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)    
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'email'
+    
